11f38e6904c76c19b6ca432c83fd81d8f50a1848
llathasa-veleth <llathasa@outlook.com>
2022-01-04 12:58:51 +0300

    Mix format

--- lib/balalaika_bear/longpoll/group_longpoll.ex
+++ lib/balalaika_bear/longpoll/group_longpoll.ex
@@ -7,10 +7,13 @@ defmodule BalalaikaBear.Longpoll.GroupLongpoll do
   end
 
   defp get_server(%{group_id: id, access_token: token, v: version}) do
-    {:ok, result} = BalalaikaBear.Groups.get_long_poll_server(%{
-          group_id: id,
-          access_token: token,
-          v: version})
+    {:ok, result} =
+      BalalaikaBear.Groups.get_long_poll_server(%{
+        group_id: id,
+        access_token: token,
+        v: version
+      })
+
     result
   end
 
@@ -18,33 +21,36 @@ defmodule BalalaikaBear.Longpoll.GroupLongpoll do
     response =
       HTTPoison.post(
         server,
-        URI.encode_query(
-          %{act: "a_check",
-            key: key,
-            ts: ts,
-            wait: 25}))
+        URI.encode_query(%{act: "a_check", key: key, ts: ts, wait: 25})
+      )
 
     case response do
-	    {:ok, %HTTPoison.Response{body: body}} ->
-		    Jason.decode!(body)
+      {:ok, %HTTPoison.Response{body: body}} ->
+        Jason.decode!(body)
+
       {:error, %HTTPoison.Error{reason: :timeout}} ->
         connect_longpoll(server, key, ts)
     end
   end
 
   defp process_longpoll(response, result, parent, data) do
-    response = case response["failed"] do
-	    1 ->
-		    connect_longpoll(result["server"], result["key"], response["ts"])
-      2 ->
-        result = get_server(data)
-		    connect_longpoll(result["server"], result["key"], response["ts"])
-      3 ->
-        result = get_server(data)
-		    connect_longpoll(result["server"], result["key"], result["ts"])
-      _ ->
-        connect_longpoll(result["server"], result["key"], response["ts"])
-    end
+    response =
+      case response["failed"] do
+        1 ->
+          connect_longpoll(result["server"], result["key"], response["ts"])
+
+        2 ->
+          result = get_server(data)
+          connect_longpoll(result["server"], result["key"], response["ts"])
+
+        3 ->
+          result = get_server(data)
+          connect_longpoll(result["server"], result["key"], result["ts"])
+
+        _ ->
+          connect_longpoll(result["server"], result["key"], response["ts"])
+      end
+
     send(parent, {:ok, response})
     process_longpoll(response, result, parent, data)
   end
