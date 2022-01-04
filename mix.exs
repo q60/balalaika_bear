12fd3cd2690ad924a4dfe6d0d619e1e7b078f671
llathasa-veleth <llathasa@outlook.com>
2022-01-04 13:03:10 +0300

    Update version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule BalalaikaBear.Mixfile do
   def project do
     [
       app: :balalaika_bear,
-      version: "0.1.4",
-      elixir: "~> 1.12",
+      version: "0.2.0",
+      elixir: "~> 1.13",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
